9ca6708c0122fd8d42a228353955c856275ded40
Web Dev <tom2kota@gmail.com>
2020-11-13 15:57:33 +0200

    Implement Apollo provider & Apollo client

--- src/index.jsx
+++ src/index.jsx
@@ -7,18 +7,48 @@ import {GlobalStyle} from "./globalStyles";
 import {Provider} from 'react-redux';
 import {store, reduxPersist} from "./redux/store";
 import {PersistGate} from "redux-persist/integration/react";
+import {ApolloClient, createHttpLink, InMemoryCache, gql} from "@apollo/client";
+import {ApolloProvider} from "react-apollo";
+
+const httpLink = createHttpLink({
+    uri: 'https://crwn-clothing.com'
+})
+
+const cache = new InMemoryCache()
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: cache
+})
+
+client.query({
+    query: gql`  {
+        getCollectionsByTitle(title: "hats"){
+            id
+            title
+            items{
+                id
+                name
+                price
+                imageUrl
+            }
+        }
+    }`
+}).then(r => console.log('====> GraphQl Server Data: ',r))
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <GlobalStyle/>
-                <PersistGate persistor={reduxPersist}>
-                    <App/>
-                </PersistGate>
-            </BrowserRouter>
-        </Provider>
-    </React.StrictMode>
+    <ApolloProvider client={client}>
+        <React.StrictMode>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <GlobalStyle/>
+                    <PersistGate persistor={reduxPersist}>
+                        <App/>
+                    </PersistGate>
+                </BrowserRouter>
+            </Provider>
+        </React.StrictMode>
+    </ApolloProvider>
     , document.getElementById('root')
 );
 
