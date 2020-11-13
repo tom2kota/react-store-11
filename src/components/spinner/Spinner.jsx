d68cd4f658990c257a0fe2f4dfe259bf36c4de7e
Web Dev <tom2kota@gmail.com>
2020-11-13 15:12:08 +0200

    Add Spinner component

---
+++ src/components/spinner/Spinner.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Spinner.scss';
+
+export const Spinner = () => (
+    <div className='spinner-overlay'>
+        <div className='spinner-container'/>
+    </div>
+)
